2c9c5fc428ff3dd28094b412acf457b1e6562eca
Jerry Leung <jerrytfleung@users.noreply.github.com>
2023-08-21 17:48:30 -0700

    NH-55602 - PHP: Support Laravel 7 (#12)
    * Updated for laravel 7

---
+++ 7/app/Listeners/NosetestSyncedListener.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NoseEvent;
+
+class NosetestSyncedListener
+{
+    /**
+     * Handle nosetest events.
+     */
+    public function handleNoseEvent($event)
+    {
+        usleep(1000);
+    }
+
+    /**
+     * Register the listeners for the subscriber.
+     *
+     * @param App\Events\NoseEvent $events
+     * @return void
+     */
+    public function subscribe($events)
+    {
+        /**
+         * The nosetest can only instrument the default handle function of the listener.
+         * It cannot instrument the user-defined handle function from subscriber.
+         */
+        // return [
+        //     NoseEvent::class => 'handleNoseEvent',
+        // ];
+        $events->listen(
+            'App\Events\NoseEvent',
+            'App\Listeners\NosetestSyncedListener@handleNoseEvent'
+        );
+    }
+}
