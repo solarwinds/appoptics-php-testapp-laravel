050a90b18f01812a44195e9405568792295f58f3
Jerry Leung <tingfung.leung@solarwinds.com>
2024-08-07 16:32:45 -0700

    Added laravel 11 (signed)

---
+++ 11/app/Listeners/NosetestQueuedListener.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\NoseEvent;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class NosetestQueuedListener implements ShouldQueue
+{
+    public function handle(NoseEvent $event)
+    {
+        usleep(1000);
+    }
+}
---
+++ 11/routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+use App\Http\Controllers\TestController;
+
+Route::get('/nosetest', [TestController::class, 'nosetest']);
+Route::get('/nosetesterror', [TestController::class, 'nosetestError']);
+
