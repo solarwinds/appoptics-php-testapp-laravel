7b1ad7c631650a7c5ba617e701b5c6eb6dc581d0
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-22 16:14:08 -0700

    Tried invalid host

--- 13/bootstrap/app.php
+++ 13/bootstrap/app.php
@@ -13,7 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-        $middleware->trustHosts(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
