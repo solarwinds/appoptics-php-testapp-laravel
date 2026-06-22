fa68b5a38146193422ad9a97e25a54c2e1e5b695
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-22 14:08:30 -0700

    Try trust proxies

--- 13/bootstrap/app.php
+++ 13/bootstrap/app.php
@@ -13,11 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-/*
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
-*/
     })->create();
