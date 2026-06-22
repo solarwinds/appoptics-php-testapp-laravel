b6c098f4453d98b4053f6d5c209f9305a0f9d519
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-22 13:02:49 -0700

    updated

--- 13/bootstrap/app.php
+++ 13/bootstrap/app.php
@@ -15,7 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
+/*
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*'),
         );
+*/
     })->create();
