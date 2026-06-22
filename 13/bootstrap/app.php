63697ec5754d024370054294b4d4d7075ff3b85a
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-22 14:56:51 -0700

    Trusted hosts

--- 13/bootstrap/app.php
+++ 13/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-        $middleware->trustProxies(at: '*');
+        $middleware->trustHosts(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
