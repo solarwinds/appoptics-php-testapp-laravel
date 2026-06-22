1cf9ca6e00f06cfb9fb61b1a24e74bb57496c542
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-22 15:36:04 -0700

    Fixed array

--- 13/bootstrap/app.php
+++ 13/bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
-        $middleware->trustHosts(at: '*');
+        $middleware->trustHosts(at: ['*']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
