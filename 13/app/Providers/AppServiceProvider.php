d2e697e088456604eff5b388e131a015e52bc937
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-22 14:24:48 -0700

    Synced Listener

--- 13/app/Providers/AppServiceProvider.php
+++ 13/app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use App\Listeners\NosetestSyncedListener;
+use Illuminate\Support\Facades\Event;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::subscribe(NosetestSyncedListener::class);
     }
 }
