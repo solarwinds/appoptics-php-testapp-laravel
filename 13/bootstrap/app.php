fa59bb1e48ba41bd1fff3a2cc268104410d1eb4e
Jerry Leung <tingfung.leung@solarwinds.com>
2026-06-22 20:48:24 -0700

    updated

--- 13/bootstrap/app.php
+++ 13/bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
+        $middleware->remove(\Illuminate\Http\Middleware\TrustHosts::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
